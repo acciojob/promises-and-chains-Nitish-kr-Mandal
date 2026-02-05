9817636d7fe6ec91839867329877232e8436636d
Nitish Kumar Mandal <147975324+Nitish-kr-Mandal@users.noreply.github.com>
2026-02-05 19:20:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+ const form = document.getElementById("form");
+
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const age = document.getElementById("age").value;
+      const name = document.getElementById("name").value;
+
+      // Validation
+      if (age === "" || name === "") {
+        alert("Please enter valid details.");
+        return;
+      }
+
+      const checkAge = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (age > 18) {
+            resolve(`Welcome, ${name}. You can vote.`);
+          } else {
+            reject(`Oh sorry ${name}. You aren't old enough.`);
+          }
+        }, 4000);
+      });
+
+      checkAge
+        .then((message) => {
+          alert(message);
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    });
